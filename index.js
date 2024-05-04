703cb297ffb2a76f36ea7cd01dfc772a91c42323
gta-pong <nomadpong@gmail.com>
2024-05-04 15:49:25 +0800

    passing 2nd test with correct responseObj

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// app.use(express.json());
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use('/api/shorturl', bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/shorturl", bodyParser.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
 app.use("/public", express.static(`${process.cwd()}/public`));
-
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
@@ -24,23 +21,16 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-//LEFT: Test post with postman
-app.post('/api/shorturl', function (req, res) {
+//LEFT: complete last 2 tests
+app.post("/api/shorturl", function (req, res) {
   console.log("api/shorturl fired");
   console.log(req.body);
-  // console.log(req.query);
-  // let url = req.query.url;
-  // console.log(url);
-  // res.json({ requestBody: req.body });
-  res.json({ test: "test1" });
+  let responseObj = {};
+  responseObj.original_url = req.body.url;
+  responseObj.short_url = 1;
+  console.log(responseObj);
+  res.json(responseObj);
 });
-
-//JUST FOR TESTING
-// app.get('/api/shorturl', function(req, res, next) {
-//   console.log('api/shorturl fired');
-//   res.json({ test: 'test1' });
-// }
-// );
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
